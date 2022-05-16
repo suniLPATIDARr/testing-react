7560813d0ce08e82bcc47710201e14e11909abb1
suniLPATIDARr <sunilpatidar1209@gmail.com>
2022-05-16 22:45:13 +0530

    intial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <label for="username-input">Username</label>
+      <input id="username-input" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { getByLabelText} from '@testing-library/dom'
+
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Link/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('comps', () => {
+  render(<App />);
+
+})
+// With screen:
+const inputNode1 = screen.getByLabelText('Username')
+
+// Without screen, you need to provide a container:
